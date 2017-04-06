389e8396085b6ffa80446dc409ed34cfbf1d2489
Fabien MARROT <fmarrot@pixelus.fr>
2017-04-06 11:30:01 +0200

    Add sage 8.5.1

---
+++ lib/extras.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Roots\Sage\Extras;
+
+use Roots\Sage\Setup;
+
+/**
+ * Add <body> classes
+ */
+function body_class($classes) {
+  // Add page slug if it doesn't exist
+  if (is_single() || is_page() && !is_front_page()) {
+    if (!in_array(basename(get_permalink()), $classes)) {
+      $classes[] = basename(get_permalink());
+    }
+  }
+
+  // Add class if sidebar is active
+  if (Setup\display_sidebar()) {
+    $classes[] = 'sidebar-primary';
+  }
+
+  return $classes;
+}
+add_filter('body_class', __NAMESPACE__ . '\\body_class');
+
+/**
+ * Clean up the_excerpt()
+ */
+function excerpt_more() {
+  return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'sage') . '</a>';
+}
+add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
+
+/**
+ * Custom print function
+ */
+function pr($data){
+  $return = print_r('<pre>');
+  $return.= print_r($data);
+  $return.= print_r('</pre>');
+  return $return;
+}
+
+/**
+ * ACF Options()
+ */
+// function my_acf_init() {
+// 	acf_update_setting('google_api_key', get_field('gmap_key','option'));
+// }
+//
+// add_action('acf/init', __NAMESPACE__ . '\\my_acf_init');
+
+/**
+ * Avoid accents in media upload
+ */
+function sanitize_filename_on_upload($filename) {
+  $ext = end(explode('.',$filename));
+  // Replace all weird characters
+  $sanitized = preg_replace('/[^a-zA-Z0-9-_.]/','', substr($filename, 0, -(strlen($ext)+1)));
+  // Replace dots inside filename
+  $sanitized = str_replace('.','-', $sanitized);
+  return strtolower($sanitized.'.'.$ext);
+}
+
+add_filter('sanitize_file_name', __NAMESPACE__ . '\\sanitize_filename_on_upload', 10);
+
+/**
+ * Add new image sizes to post or page editor
+ */
+function oxbow_image_sizes( $sizes ) {
+    $sizes = array_merge( $sizes, array(
+      'post-image' => __( 'Image de contenu' )
+    ));
+    return $sizes;
+}
+add_filter( 'image_size_names_choose', __NAMESPACE__ . '\\oxbow_image_sizes' );
+
+/**
+ * Remove menu items for other admins (SEO/WPML/ACF/Tools/Users/Options/Plugins)
+ */
+// add_action('admin_menu', __NAMESPACE__ . '\\remove_admin_menu_links');
+// function remove_admin_menu_links(){
+//   $user = wp_get_current_user();
+//   if( $user && isset($user->ID) && $user->ID != 1 ) {
+//     remove_menu_page('options-general.php');
+//     remove_menu_page('plugins.php');
+//     remove_menu_page('users.php');
+//     remove_menu_page('tools.php');
+//     remove_menu_page('edit.php?post_type=acf-field-group');
+//     remove_menu_page('admin.php?page=sitepress-multilingual-cms');
+//     remove_menu_page('sitepress-multilingual-cms/menu/languages.php');
+//     remove_menu_page('wpseo_dashboard');
+//   }
+// }
